fe1687e7247e5c6ecada55614bd14750d54b9586
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-03-24 17:37:29 +0100

    feat: add server

--- src/main/java/themeparkcraft.core.api/Core.java
+++ src/main/java/themeparkcraft.core.api/Core.java
@@ -1,7 +1,34 @@
 package themeparkcraft.core.api;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import themeparkcraft.core.api.server.Server;
+
+import javax.annotation.Nonnull;
+
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public final class Core {
 
+    private static CoreServer server;
 
+    /**
+     * Get the JavaPlugin instance of CorePlugin.
+     * <br>
+     * This gives you access to some Bukkit features that rely on this instance.
+     *
+     * @return JavaPlugin instance of CorePlugin
+     */
+    @Nonnull
+    public static JavaPlugin getInstance() {
+        return server.getInstance();
+    }
+
+    /**
+     * Get the {@link Server} object, granting access to all server-related features.
+     *
+     * @return Server object
+     */
+    @Nonnull
+    public static Server server() {
+        return server.server();
+    }
 }
--- src/main/java/themeparkcraft.core.api/CoreServer.java
+++ src/main/java/themeparkcraft.core.api/CoreServer.java
@@ -1,7 +1,17 @@
 package themeparkcraft.core.api;
 
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.ApiStatus;
+import themeparkcraft.core.api.server.Server;
+
+import javax.annotation.Nonnull;
 
 @ApiStatus.Internal
 public interface CoreServer {
+
+    @Nonnull
+    JavaPlugin getInstance();
+
+    @Nonnull
+    Server server();
 }
